02cb082b239d7cf920e17f3f52e8a0b56f03fdaf
Simeon Grozdev <moni.grozdev@gmail.com>
2023-11-04 13:03:33 +0200

    Add homework.

---
+++ JS/JS Arrays and Strings/modern-times-of-#(hashtag).js
@@ -0,0 +1,9 @@
+function modernTimesOfHashtag(string) {
+    const regex = RegExp('#([a-z]+)(?: |$)', 'ig')
+
+    const result = Array.from(string.matchAll(regex))
+
+    result.forEach((value) => {console.log(value[1])})
+}
+
+modernTimesOfHashtag('#Nowa#days everyone uses # to tag a #special word in #socialMedia')

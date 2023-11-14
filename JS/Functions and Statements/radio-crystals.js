7e9ac1ac8dd3063e2ef74df51e9bf39b58d861b2
Simeon Grozdev <moni.grozdev@gmail.com>
2023-11-14 15:44:34 +0200

    Add homework.

---
+++ JS/Functions and Statements/radio-crystals.js
@@ -0,0 +1,54 @@
+function radioCrystals(array) {
+
+    function processCrystalRecursion(crystal, operation, desiredThickness, counter) {
+        let result = operation(crystal)
+        if (result < desiredThickness - 1) {
+            return [crystal, counter]
+        }
+        return processCrystalRecursion(operation(crystal), operation, desiredThickness, counter + 1)
+    }
+
+    const operationsDecrease = {
+        'Cut': (x) => x * 0.25,
+        'Lap': (x) => x * 0.8,
+        'Grind': (x) => x - 20,
+        'Etch': (x) => x - 2,
+    }
+
+    const operationsIncreaseAndWash = {
+        'X-ray': (x) => x + 1,
+        'Transporting and washing': (x) => Math.floor(x)
+    }
+
+    let [finalThicknessGoal, ...crystals] = array;
+
+    for (let crystal of crystals) {
+        console.log(`Processing chunk ${crystal} microns`)
+        let usedLaser = false
+
+        for (const operation in operationsDecrease) {
+            let [operationResult, repeated] = processCrystalRecursion(crystal, operationsDecrease[operation], finalThicknessGoal, 0)
+
+            if (operationResult !== crystal) {
+                crystal = operationResult
+                console.log(`${operation} x${repeated}`)
+
+                crystal = operationsIncreaseAndWash["Transporting and washing"](crystal)
+                console.log('Transporting and washing')
+            }
+
+            if (crystal < finalThicknessGoal) {
+                crystal = operationsIncreaseAndWash["X-ray"](crystal)
+                usedLaser = true
+                console.log('X-ray x1')
+            }
+
+            if (crystal === finalThicknessGoal) {
+                console.log(`Finished crystal ${finalThicknessGoal} microns`)
+                break
+            }
+        }
+    }
+}
+
+radioCrystals([1000, 4000, 8100])

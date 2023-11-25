8505b26c28d114a59a359e55a1c5ba4a926c5050
Simeon Grozdev <moni.grozdev@gmail.com>
2023-11-25 14:00:13 +0200

    Add homework.

--- JS/DOM Introduction/Table - Search Engine/solution.js
+++ JS/DOM Introduction/Table - Search Engine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+    function onClick() {
+        const searchField = document.getElementById('searchField');
+        const tableRows = document.querySelectorAll('tbody tr');
 
-   }
+        for (const tableRow of tableRows) {
+            const tableCells = Array.from(tableRow.children)
+            const found = tableCells.some(cell => cell.textContent.includes(searchField.value))
+
+            if (found) {
+                tableRow.classList.add('select');
+            } else {
+                tableRow.classList.remove('select');
+            }
+        }
+    }
 }

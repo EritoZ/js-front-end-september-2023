cb5672e0417548b44313949b4ab1e8bd69d71aa4
Simeon Grozdev <moni.grozdev@gmail.com>
2023-11-25 18:15:40 +0200

    Improve homework.

--- JS/DOM Introduction/Table - Search Engine/solution.js
+++ JS/DOM Introduction/Table - Search Engine/solution.js
@@ -5,11 +5,12 @@ function solve() {
         const searchField = document.getElementById('searchField');
         const tableRows = document.querySelectorAll('tbody tr');
 
-        for (const tableRow of tableRows) {
-            const tableCells = Array.from(tableRow.children)
-            const found = tableCells.some(cell => cell.textContent.includes(searchField.value))
+        if (searchField.value === '') {
+            return;
+        }
 
-            if (found) {
+        for (const tableRow of tableRows) {
+            if (tableRow.textContent.includes(searchField.value)) {
                 tableRow.classList.add('select');
             } else {
                 tableRow.classList.remove('select');

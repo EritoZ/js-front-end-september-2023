a5e66e3c4312cf0390c419196903eef06c025c4c
Simeon Grozdev <moni.grozdev@gmail.com>
2023-11-26 13:22:04 +0200

    Add homework.

--- JS/DOM Introduction/Format the Text/solution.js
+++ JS/DOM Introduction/Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+    const textInput = document.getElementById('input');
+    const result = document.getElementById('output');
+    result.innerHTML = ''
+
+    let formatedText = textInput.value.split('.');
+
+    if (!formatedText[formatedText.length - 1]) {
+        formatedText.pop();
+    }
+
+    for (let i = 0; i < formatedText.length; i += 3) {
+        const paragraph = formatedText.slice(i, i + 3).join('. ');
+
+        const p = document.createElement('p');
+
+        p.textContent = `${paragraph}.`;
+
+        result.appendChild(p);
+    }
 }

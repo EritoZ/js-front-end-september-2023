659c2f76969950590b1369976f1620543185d1a3
Simeon Grozdev <moni.grozdev@gmail.com>
2023-12-03 12:43:53 +0200

    Add homework.

--- JS/DOM Manipulation and Events/Locked-Profile/app.js
+++ JS/DOM Manipulation and Events/Locked-Profile/app.js
@@ -1,3 +1,28 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttonsDisplay = document.querySelectorAll('.profile button:last-child');
+
+    for (const button of buttonsDisplay) {
+        button.addEventListener('click', toggleShowInformation);
+    }
+
+    function toggleShowInformation(e) {
+        const buttonDisplay = e.currentTarget;
+        const hiddenInfo = buttonDisplay.previousElementSibling;
+        const displayStatus = hiddenInfo.style.display;
+
+        const profile = buttonDisplay.parentNode;
+        const lockButton = profile.querySelector('input[value="lock"]')
+
+        if (lockButton.checked) {
+            return;
+        }
+
+        if (displayStatus === '') {
+            hiddenInfo.style.display = 'block';
+            buttonDisplay.textContent = 'Hide it';
+        } else {
+            hiddenInfo.style.display = '';
+            buttonDisplay.textContent = 'Show more';
+        }
+    }
 }
--- JS/DOM Manipulation and Events/Time-Converter/solution.js
+++ JS/DOM Manipulation and Events/Time-Converter/solution.js
@@ -1,4 +1,56 @@
 function attachEventsListeners() {
+    const buttonsTakeTime = document.querySelectorAll('div input:last-child');
 
-    console.log('TODO:...');
+    for (const button of buttonsTakeTime) {
+        button.addEventListener('click', convertTime);
+    }
+
+    function convertTime(e) {
+        const valueTime = e.currentTarget.previousElementSibling;
+        const timeType = valueTime.id;
+
+        convertToDays(timeType, Number(valueTime.value));
+    }
+
+    function convertToDays(timeType, valueTime) {
+        switch (timeType) {
+            case 'days':
+                break;
+            case 'hours':
+                valueTime /= 24;
+                break;
+            case 'minutes':
+                valueTime /= 60 * 24;
+                break;
+            case 'seconds':
+                valueTime /= 60 * 60 * 24;
+                break;
+        }
+
+        buttonsTakeTime[0].previousElementSibling.value = valueTime;
+
+        convertToHours(valueTime);
+    }
+
+    function convertToHours(valueTime) {
+        valueTime *= 24;
+
+        buttonsTakeTime[1].previousElementSibling.value = valueTime;
+
+        convertToMinutes(valueTime);
+    }
+
+    function convertToMinutes(valueTime) {
+        valueTime *= 60;
+
+        buttonsTakeTime[2].previousElementSibling.value = valueTime;
+
+        convertToSeconds(valueTime);
+    }
+
+    function convertToSeconds(valueTime) {
+        valueTime *= 60;
+
+        buttonsTakeTime[3].previousElementSibling.value = valueTime;
+    }
 }

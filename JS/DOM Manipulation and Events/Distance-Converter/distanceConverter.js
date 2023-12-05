a383729dba2c8a8d336d53e517aa7c587210f75f
Simeon Grozdev <moni.grozdev@gmail.com>
2023-12-05 12:56:55 +0200

    Rework homework.

--- JS/DOM Manipulation and Events/Distance-Converter/distanceConverter.js
+++ JS/DOM Manipulation and Events/Distance-Converter/distanceConverter.js
@@ -10,30 +10,18 @@ function attachEventsListeners() {
         const outputUnit = document.getElementById('outputUnits');
         const outputDistance = outputUnit.previousElementSibling;
 
-        const convertDistanceToMeters = {
-            'km': (distance) => distance * 1000,
-            'm': (distance) => distance * 1,
-            'cm': (distance) => distance * 0.01,
-            'mm': (distance) => distance * 0.001,
-            'mi': (distance) => distance * 1609.34,
-            'yrd': (distance) => distance * 0.9144,
-            'ft': (distance) => distance * 0.3048,
-            'in': (distance) => distance * 0.0254,
+        const conversionRateToMeters = {
+            'km': 1000,
+            'm': 1,
+            'cm': 0.01,
+            'mm': 0.001,
+            'mi': 1609.34,
+            'yrd': 0.9144,
+            'ft': 0.3048,
+            'in': 0.0254,
         }
 
-        const convertMetersToDistance = {
-            'km': (distance) => distance / 1000,
-            'm': (distance) => distance / 1,
-            'cm': (distance) => distance / 0.01,
-            'mm': (distance) => distance / 0.001,
-            'mi': (distance) => distance / 1609.34,
-            'yrd': (distance) => distance / 0.9144,
-            'ft': (distance) => distance / 0.3048,
-            'in': (distance) => distance / 0.0254,
-        }
-
-        const meters = convertDistanceToMeters[inputUnit.value](inputDistance.value);
-
-        outputDistance.value = convertMetersToDistance[outputUnit.value](meters);
+        outputDistance.value = inputDistance.value * conversionRateToMeters[inputUnit.value] /
+            conversionRateToMeters[outputUnit.value];
     }
 }
